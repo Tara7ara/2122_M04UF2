ed6a732329e1da867849cdeb0ca153831f426ed7
enti <enti@enti.enti>
2022-04-01 13:11:05 +0200

    cosas de clase pero me falla el boton

--- todolist/src/RemoveTask.js
+++ todolist/src/RemoveTask.js
@@ -1,79 +1,69 @@
 import React from 'react';
 
 import Button from '@mui/material/Button';
+
 import DeleteIcon from '@mui/icons-material/Delete';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-
+import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
-class RemoveTask extends React.Component{
-
+class RemoveTask extends React.Component 
+{
 	constructor (props){
 		super(props);
-		this.state = { 
-						open: false
-					 };
-	
+
+		this.state = {
+			open: false
 		}
+	}
 
-	handleOnClick = ()=>{
-	this.setState({open: true });
+	openDialog = () => {
+		this.state.open = true;
+		this.setState({
+			open: this.state.open
+		});
+	};
 
-}
+	closeDialog = () => {
+		this.state.open = false;
+		this.setState({
+			open: this.state.open
+		});
+	};
+
+	removeTask = () => {
+		this.props.removeTask(this.props.id_task);
+		this.closeDialog();
+	};
 
 
-	handleClose = () => {
-	this.setState({open:false});
-}
-
-	handleDelete = () =>{
-	this.props.removeTask(this.props.task_id);
-	this.setState({open:false});
-
-}
 
 
- render() {
- 	return (
-	<div>
-		<Button variant="contained" 
-				startIcon=<DeleteIcon /> 
-				onClick={this.handleOnClick}>	
 
-		BORRAR
-		 
-		</Button>
+	render (){
+		return (
+		<div>
+		<Button startIcon={<DeleteIcon />} onClick={this.openDialog}>BORRAR</Button>
 
-		<Dialog  
-			open={this.state.open}
-			onClose={this.handleClose}
-			>
-			<DialogTitle>
-				{'Estas seguro de borrar?'}
-					</DialogTitle>
-
-			<DialogContent>
-			</DialogContent>
-
-			<DialogActions>
-				<Button onClick={this.handleDelete}>
-					BORRAR
-						</Button>
-				<Button onClick={this.handleClose}> 
-
-					Salir
-					</Button>
-
-			</DialogActions>
+		<Dialog open={this.state.open} onClose={this.closeDialog}>
+		<DialogTitle>Borrar tarea</DialogTitle>
+		<DialogContent>
+		<DialogContentText>
+			¿Quieres borrar "NOMBRE TAREA"?
+		</DialogContentText>
+		</DialogContent>
+		<DialogActions>
+		<Button onClick={this.closeDialog}>No</Button>
+		<Button startIcon={<DeleteIcon />} variant="contained" color="error" onClick={this.removeTask}>Sí</Button>
+		</DialogActions>
 		</Dialog>
-</div>
+		</div>
 
-
-
-);
+		);
+	}
 
 }
-}
+
 export default RemoveTask;
--- todolist/src/TaskSubmit.js
+++ todolist/src/TaskSubmit.js
@@ -1,33 +1,11 @@
-import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 
-import React from 'react';
-
-class  TaskSubmit extends React.Component{
-	constructor (props){
-	
-		super(props);
-
-	}
-handleSubmit = (event) => {
-	event.preventDefault();
-	this.props.onSubmit(event);
-
-}
-
-render(){
-	return(
-	
-	<Tooltip title="A(ny)adir tarea" >
-		<Button variant="contained" onClick={this.handleSubmit}>
-			<AddCircleIcon />
-				</Button>
-	</Tooltip>
+function TaskSubmit ()
+{
+	return (
+		<Button variant="contained">Añadir</Button>
 	);
 
-
-}
 }
 
 export default TaskSubmit;
